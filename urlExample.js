1f8789541101481086c7c5b0f230739b77299139
warndarkahead <ayush.it.21023@recb.ac.in>
2023-11-18 07:16:42 +0530

    parse url

---
+++ urlExample.js
@@ -0,0 +1,15 @@
+const http = require('http');
+const url = require('url');
+
+//url.parse()
+
+http.createServer((request,response)=>{
+
+    console.log(request.url);
+
+    const urlOb = url.parse(request.url,true);
+    console.log(urlOb);
+    console.log(urlOb.query.keyword);
+
+
+}).listen(8082);
